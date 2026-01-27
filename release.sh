915f048aedcc098da1c4d0995d588350f823700b
Sergii Mavrov <serg.mavrov@gmail.com>
2026-01-27 20:07:43 +0100

    Release v1.0.93: Split Publish Jobs.

--- release.sh
+++ release.sh
@@ -36,7 +36,7 @@ fi
 # 3. Commit synchronized version files
 echo ""
 echo "Step 3: Committing version updates..."
-DESC="Release v$VERSION: CI Refactor (Build-Test-Deploy)."
+DESC="Release v$VERSION: Split Publish Jobs."
 git add .
 git commit -m "$DESC"
 
